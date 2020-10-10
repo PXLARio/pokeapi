f5f4e2069344c9492b230d56d31ae5f8e752a142
terribledevice <longlost@gmail.com>
2020-10-10 13:56:16 -0400

    passing stats successfully now

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,11 @@ pokemon_selector_one.addEventListener("change", function() {
             pokemon_sprite.setAttribute('alt', pokemon_name);
             pokemon_title.innerHTML = pokemon_name;
             document.getElementsByClassName("pokemon-sprite-one")[0].appendChild(pokemon_sprite);
+
+            document.getElementById("base-xp-1").innerHTML = `XP: ${data.base_experience}`;
+            document.getElementById("poke-hp-1").innerHTML = `HP: ${data.stats[0].base_stat}`;
+            document.getElementById("poke-attack-1").innerHTML = `Attack: ${data.stats[1].base_stat}`;
+            document.getElementById("poke-def-1").innerHTML = `Defense: ${data.stats[2].base_stat}`;
         })
 })
 
@@ -52,6 +57,10 @@ pokemon_selector_two.addEventListener("change", function() {
             pokemon_sprite.setAttribute('alt', pokemon_name);
             pokemon_title.innerHTML = pokemon_name;
             document.getElementsByClassName("pokemon-sprite-two")[0].appendChild(pokemon_sprite);
+
+            document.getElementById("base-xp-2").innerHTML = `XP: ${data.base_experience}`;
+            document.getElementById("poke-hp-2").innerHTML = `HP: ${data.stats[0].base_stat}`;
+            document.getElementById("poke-attack-2").innerHTML = `Attack: ${data.stats[1].base_stat}`;
+            document.getElementById("poke-def-2").innerHTML = `Defense: ${data.stats[2].base_stat}`;
         })
-    document.getElementsByClassName("pokemon-sprite-two").innerHTML = "";
 })
