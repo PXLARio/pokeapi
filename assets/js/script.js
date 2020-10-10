ab3dcea04919e24c11ea1560831f6ffcfde84f22
terribledevice <longlost@gmail.com>
2020-10-10 08:28:19 -0400

    selector drop down now displays pokemon sprite

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,52 @@
-<<<<<<< HEAD
-fetch_pokemon = () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=100`)
-        .then(function(response) {
-            response.json()
-                .then(function(pokemon) {
-                    let pokemon_list_one = pokemon.results;
-                    let pokemon_selector_one = document.getElementById('pokemon-dropdown-one');
-                    pokemon_list_one.map(function(pokemon) {
-                        let option_list_one = document.createElement('option');
-                        option_list_one.setAttribute('value', pokemon);
-                        option_list_one.innerHTML = pokemon.name;
-                        pokemon_selector_one.appendChild(option_list_one);
-                    });
-                    let pokemon_list_two = pokemon.results;
-                    let pokemon_selector_two = document.getElementById('pokemon-dropdown-two');
-                    pokemon_list_two.map(function(pokemon) {
-                        let option_list_two = document.createElement('option');
-                        option_list_two.setAttribute('value', pokemon);
-                        option_list_two.innerHTML = pokemon.name;
-                        pokemon_selector_two.appendChild(option_list_two);
-                    });
-                });
-        });
-};
-
-fetch_pokemon();
-
-
-=======
-const P = new Pokedex.Pokedex();
-
-var interval = {
-    limit: 100,
-    offset: 0
-}
-
-P.getPokemonsList(interval)
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=30`)
     .then(function(response) {
-        return response;
-    }).then(function(response) {
-
-
-
-        // let pokemonListOne = document.getElementById('pokemon-dropdown-one');
-        // playerOne.map(function(pokemon) {
-        //     let pokeElementOne = document.createElement('option');
-        //     pokeElementOne.setAttribute('value', pokemon);
-        //     pokeElementOne.innerHTML = pokemon.name;
-        //     pokemonListOne.appendChild(pokeElementOne);
+        response.json()
+            .then(function(pokemon) {
+                let pokemon_list_one = pokemon.results;
+                let pokemon_selector_one = document.getElementById('pokemon-dropdown-one');
+                pokemon_list_one.map(function(pokemon) {
+                    let option_list_one = document.createElement('option');
+                    option_list_one.setAttribute('value', pokemon.name);
+                    option_list_one.innerHTML = pokemon.name;
+                    pokemon_selector_one.appendChild(option_list_one);
+                });
+                let pokemon_list_two = pokemon.results;
+                let pokemon_selector_two = document.getElementById('pokemon-dropdown-two');
+                pokemon_list_two.map(function(pokemon) {
+                    let option_list_two = document.createElement('option');
+                    option_list_two.setAttribute('value', pokemon.name);
+                    option_list_two.innerHTML = pokemon.name;
+                    pokemon_selector_two.appendChild(option_list_two);
+                });
+            });
     });
-// let playerTwo = data.results;
-// let pokemonListTwo = document.getElementById('pokemon-dropdown-two');
-// playerTwo.map(function(pokemon) {
-// let pokeElementTwo = document.createElement('option');
-// pokeElementTwo.setAttribute('value', pokemon);
-// pokeElementTwo.innerHTML = pokemon.name;
-// pokemonListTwo.appendChild(pokeElementTwo);
-// });
-// })
 
 
 
-// fetch(`https://pokeapi.co/api/v2/pokemon?limit=100`)
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//         let playerOne = data.results.sort();
-//         let pokemonListOne = document.getElementById('pokemon-dropdown-one');
-//         playerOne.map(function(pokemon) {
-//             let pokeElementOne = document.createElement('option');
-//             pokeElementOne.setAttribute('value', pokemon);
-//             pokeElementOne.innerHTML = pokemon.name;
-//             pokemonListOne.appendChild(pokeElementOne);
-//         });
-//         let playerTwo = data.results.sort();
-//         let pokemonListTwo = document.getElementById('pokemon-dropdown-two');
-//         playerTwo.map(function(pokemon) {
-//             let pokeElementTwo = document.createElement('option');
-//             pokeElementTwo.setAttribute('value', pokemon);
-//             pokeElementTwo.innerHTML = pokemon.name;
-//             pokemonListTwo.appendChild(pokeElementTwo);
-//         });
-//     })
->>>>>>> fa47090157f284a06ac74c87f51019a8e4730005
+let pokemon_selector_one = document.getElementById("pokemon-dropdown-one");
+pokemon_selector_one.addEventListener("change", function() {
+    let pokemon_name = this.value;
+    // console.log(pokemon_name);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_name}`)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            let pokemon_sprite = document.createElement('img');
+            pokemon_sprite.setAttribute('src', data.sprites["front_default"]);
+            document.getElementsByClassName("pokemon-sprite-one")[0].appendChild(pokemon_sprite);
+        })
+})
 
-
-// pokeSelector.addEventListener("change", function() {
-//     let breedName = this.value;
-//     fetch(`https://pokeapi.co/api/v2/pokemon/`)
-//         .then(function(response) {
-//             return response.json();
-//         }).then(function(data) {
-//             let breedImage = document.createElement('img');
-//             breedImage.setAttribute('src', data.message);
-//             document.body.appendChild(breedImage);
-
-
-//         })
-
-// })
+let pokemon_selector_two = document.getElementById("pokemon-dropdown-two");
+pokemon_selector_two.addEventListener("change", function() {
+    let pokemon_name = this.value;
+    // console.log(pokemon_name);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_name}`)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            let pokemon_sprite = document.createElement('img');
+            pokemon_sprite.setAttribute('src', data.sprites["front_default"]);
+            document.getElementsByClassName("pokemon-sprite-two")[0].appendChild(pokemon_sprite);
+        })
+})
